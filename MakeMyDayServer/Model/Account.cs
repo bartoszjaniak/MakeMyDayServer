783b1041539431df0a50ec580bfc8fcf36d87148
bartoszjaniak <bartoszjaniak@outlook.com>
2018-01-24 23:49:10 +0100

    Dodaj pliki projektów.

---
+++ MakeMyDayServer/Model/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeMyDayServer.Model
+{
+    public class Account
+    {
+        public enum CreateStatus
+        {
+            OK,
+            EXISTS,
+            ERROR
+        };
+
+        public string Login { get; }
+        public string Password { get; }
+        public string SuperToken { get; }
+        
+
+        //public CreateStatus Account(string login, string password)
+        //{
+        //    Login = login;
+        //    Password = password;
+        //    SuperToken = GenerateSuperToken();
+
+        //    return CreateStatus.OK;
+        //}
+
+        private string GenerateSuperToken()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public bool TryLogin()
+        {
+            //TODO: check in database if user is exists and check password
+            return true;
+        }
+
+
+    }
+}
---
+++ MakeMyDayServer/Model/Token.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeMyDayServer.Model
+{
+    public class Token
+    {
+        public Guid Value { get; private set; }
+        public DateTime DeadTime { get; private set; }
+
+        public Token()
+        {
+            if(Value == null)
+            {
+                Value = Guid.NewGuid();
+                DeadTime = DateTime.Now.AddMinutes(20);
+            }           
+        }
+
+        public bool CheckExpirationTime()
+        {
+            if (DeadTime > DateTime.Now)
+                return true;            
+            else
+                return false;
+        }
+    }
+}
